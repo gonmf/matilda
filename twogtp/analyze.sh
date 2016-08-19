eb9d463c880ee98e2be33dc3b54c9c778a5478e4
gonmf <gonmf@sapo.pt>
2016-08-20 00:19:21 +0100

    Correct README

--- twogtp/analyze.sh
+++ twogtp/analyze.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 gogui-twogtp -analyze ./output.dat
-echo "WR for matilda (black):"
-cat output.html | grep "Black wins:" -F
+cat output.html | grep -E 'Black wins|<h2>Result'
 cat output.html | grep "Games:" -F
 cat output.html | grep "Games used:" -F
 cat output.html | grep ">Time Black:" -F
