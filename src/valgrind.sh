dc38fd4844963f57c3540ab770a219e2a293554f
gonmf <gonmf@sapo.pt>
2016-09-27 22:38:30 +0100

    Performance improvement of opening book loading

--- src/valgrind.sh
+++ src/valgrind.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 valgrind --tool=callgrind --dump-instr=yes --simulate-cache=yes \
-    --collect-jumps=yes ./matilda -m gtp --disable_opening_books -l
+    --collect-jumps=yes ./matilda -m gtp
+    # --disable_opening_books -l
 kcachegrind
 exit 0
