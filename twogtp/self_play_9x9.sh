e03e37724a55879672751e3f48d0e9aa59d82d2f
gonmf <gonmf@sapo.pt>
2016-10-29 23:41:32 +0100

    Update twogtp scripts

--- twogtp/self_play_9x9.sh
+++ twogtp/self_play_9x9.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 rm -f output.html output.summary.dat
-BLACK="../src/matilda -m gtp -d ../src/data/ -l --disable_opening_books --playouts 1000"
-WHITE="../src/matilda-old -m gtp -d ../src/data/ -l --disable_opening_books --playouts 1000"
+BLACK="../src/matilda -m gtp -d ../src/data/ -l --disable_opening_books --playouts 10000"
+WHITE="../src/matilda-old -m gtp -d ../src/data/ -l --disable_opening_books --playouts 10000"
 REFEREE="gnugo --level 0 --mode gtp --chinese-rules --positional-superko"
-gogui-twogtp -white "$WHITE" -black "$BLACK" -referee "$REFEREE" -auto -sgffile output -games 4000 \
+gogui-twogtp -white "$WHITE" -black "$BLACK" -referee "$REFEREE" \
+-sgffile output \
+-games 4000 \
+-auto \
 -size 9 \
--komi 7.5 \
--alternate
+-komi 4.5 \
+-alternate \
 exit 0
--- twogtp/timed_self_play_19x19.sh
+++ twogtp/timed_self_play_19x19.sh
@@ -3,7 +3,10 @@ rm -f output.html output.summary.dat
 BLACK="../src/matilda -m gtp -d ../src/data/ -l --disable_opening_books"
 WHITE="../src/matilda-old -m gtp -d ../src/data/ -l --disable_opening_books"
 REFEREE="gnugo --level 0 --mode gtp --chinese-rules --positional-superko"
-gogui-twogtp -white "$WHITE" -black "$BLACK" -referee "$REFEREE" -auto -sgffile output -games 4000 \
+gogui-twogtp -white "$WHITE" -black "$BLACK" -referee "$REFEREE" \
+-sgffile output \
+-games 4000 \
+-auto \
 -size 19 \
 -komi 7.5 \
 -alternate \
