add1eb5254fe10b117fd41c927ea4cc739302e60
gonmf <gonmf@sapo.pt>
2016-11-06 09:36:36 +0000

    Simplify

--- twogtp/timed_self_play_9x9.sh
+++ twogtp/timed_self_play_9x9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f output.html output.summary.dat
-BLACK="../src/matilda -m gtp -d ../src/data/ -l d --disable_opening_books --memory 3200"
+BLACK="../src/matilda -m gtp -d ../src/data/ -l --disable_opening_books --memory 3200"
 WHITE="../src/matilda-old -m gtp -d ../src/data/ -l --disable_opening_books --memory 3200"
 REFEREE="gnugo --level 0 --mode gtp --chinese-rules --positional-superko"
 gogui-twogtp -white "$WHITE" -black "$BLACK" -referee "$REFEREE" \
