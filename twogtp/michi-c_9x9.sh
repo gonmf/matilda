2c1cff3bb9a1c7d035044eb5ce9f9d07e6631b74
gonmf <gonmf@sapo.pt>
2017-02-11 20:38:46 +0000

    Integrating michi

--- twogtp/michi-c_9x9.sh
+++ twogtp/michi-c_9x9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -f output.html output.summary.dat
 BLACK="../src/matilda -m gtp -d ../src/data/ -l e --disable_opening_books --losing resign --disable_neural_nets --threads 1 --playouts 10000"
-WHITE="../../../michi-c/michi gtp"
+WHITE="../src/michi-c gtp"
 REFEREE="gnugo --level 0 --mode gtp --chinese-rules --positional-superko"
 gogui-twogtp -white "$WHITE" -black "$BLACK" -referee "$REFEREE" \
 -sgffile output \
